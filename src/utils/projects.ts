ad635be1d69879650b07e04e8a5083c81348dd51
Philip Perez Castro <jperez.c92@gmail.com>
2022-03-25 01:48:52 -0500

    ADDED - quiz-against-time project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,4 +1,5 @@
 import connect4 from "public/projects/connect4.png";
+import quizAgainstTime from "public/projects/quiz-against-time.png";
 
 export interface Project {
   name: string;
@@ -14,5 +15,11 @@ export const projects: Project[] = [
     url: "https://connect4-steel.vercel.app/",
     repository: "https://github.com/JPerezC92/connect4",
     image: connect4,
+  },
+  {
+    name: "Quiz against time",
+    url: "https://quiz-against-time.vercel.app/",
+    repository: "https://github.com/JPerezC92/quiz-against-time",
+    image: quizAgainstTime,
   },
 ];
